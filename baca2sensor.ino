96cf44bd375c43cf71b2d15947f0a4c8f1423f99
Hasan Basri <hasanbasri1493@gmail.com>
2015-11-03 00:34:08 +0700

    Update baca2sensor.ino

--- baca2sensor.ino
+++ baca2sensor.ino
@@ -1,22 +1,23 @@
 // Panggil Liblary Sensor
 #include "DHT.h"
+#include <Wire.h>
 // Panggil Liblary LCD, RS=3, EN=4, DS4=5, DS5=6, DS6=7, DS7=8
 #include <LiquidCrystal.h>
-#include <Wire.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 
 //Macem macem variabel
 #define DHTPIN1 6    // what pin we're connected to
 #define DHTPIN2 7
-#define RelayFan1 8
-#define RelayFan2 9
-#define RelayPump 10
+#define RelayFan 8
+#define RelayPump 9
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 
 DHT sensor1(DHTPIN1, DHTTYPE);
 DHT sensor2(DHTPIN2, DHTTYPE);
 
-
+int ratasuhu; //Variabel rata rata suhu
+int ratalembab; //Variabel rata rata kelembaban
 int suhu1 = 0;
 int lembab1 = 0;
 int suhu2 = 0;
@@ -24,35 +25,15 @@ int lembab2 = 0;
 
 void setup () {
   //Setting mode pin Relay jadi output
-  pinMode(RelayFan1, OUTPUT);
-  pinMode(RelayFan2, OUTPUT);
+  pinMode(RelayFan, OUTPUT);
   pinMode(RelayPump, OUTPUT);
+  
   //Setting pin Relay jadi LOW / Mati
-  digitalWrite(RelayFan1, LOW);
-  digitalWrite(RelayFan2, LOW);
+  digitalWrite(RelayFan, LOW);
   digitalWrite(RelayPump, LOW);
   //Setting memakai lcd 16 x 2
   lcd.begin(16, 2);
 
-  // Variable starp up
-  char teks1[] = "Tugas Akhir, Rancangan Miniatur Green House Suhu dan Kelembaban dengan Menggunkan Mikrocontroller";
-  //set lcd cursor pada baris dan kolom
-  lcd.setCursor(0, 0);
-
-  lcd.print("Dean Yongkky");
-
-  lcd.autoscroll();
-  lcd.setCursor(16, 1);
-  for (int indeks = 0; indeks < 16; indeks++)
-  {
-    lcd.print(teks1[indeks]);
-    delay(500);
-  }
-  // turn off automatic scrolling
-  lcd.noAutoscroll();
-
-  // clear screen for the next loop:
-  lcd.clear();
 
   sensor1.begin();
   sensor2.begin();
@@ -64,43 +45,61 @@ void loop () {
 
   suhu1 = sensor1.readHumidity();
   lembab1 = sensor1.readTemperature();
-
-  lcd.setCursor(0, 0);
-  lcd.print("Suhu1: ");
-  lcd.print(suhu1);
-  lcd.print("C ");
-  lcd.setCursor(0, 1);
-  lcd.print("Lembab2: ");
-  lcd.print(lembab1);
-  lcd.print("%");
-
   suhu2 = sensor2.readHumidity();
   lembab2 = sensor2.readTemperature();
 
-  lcd.setCursor(0, 0);
-  lcd.print("Suhu2: ");
-  lcd.print(suhu2);
-  lcd.print("C ");
-  lcd.setCursor(0, 1);
-  lcd.print("Lembab2: ");
-  lcd.print(lembab2);
-  lcd.print("%");
- // jika suhu #mis 30 
- if(suhu1 < 30)//Sesuaikan dengan suhu yang diinginkan
+  //hitung rata rata suhu dari 2 sensor
+  ratasuhu = 2 / (suhu1 + suhu2);
+  //hitung rata rata kelembaban dari 2 sensor
+  ratalembab = 2 / ( lembab1 + lembab2);
+
+  //logika nya
+  if (ratasuhu >= 30 && ratalembab >= 40)
   {
-    //maka relay akan menyala 
-    digitalWrite(RelayPump, HIGH);//turn on the heater
+    //Nyalakan relay fan saja
+    digitalWrite(RelayFan, HIGH);//turn on the heater
 
   }
 
+  else if (ratasuhu >= 40 && ratalembab >= 50)
+
+  {
+
+    //Nyalakan kedua relay
+    digitalWrite(RelayPump, HIGH);
+    digitalWrite(RelayFan, HIGH);
+
+  }
   else
-
   {
+    lcd.setCursor(0, 0);
+    lcd.print("Suhu2: ");
+    lcd.print(suhu2);
+    lcd.print("C ");
+    lcd.setCursor(0, 1);
+    lcd.print("Lembab2: ");
+    lcd.print(lembab2);
+    lcd.print("%");
+    delay(3000);
 
-    digitalWrite(RelayPump, LOW);
-
+    lcd.setCursor(0, 0);
+    lcd.print("Suhu1: ");
+    lcd.print(suhu1);
+    lcd.print("C ");
+    lcd.setCursor(0, 1);
+    lcd.print("Lembab1: ");
+    lcd.print(lembab1);
+    lcd.print("%");
+    delay(3000);
+    lcd.setCursor(0, 0);
+    lcd.print("Suhu: ");
+    lcd.print(ratasuhu);
+    lcd.print("C ");
+    lcd.setCursor(0, 1);
+    lcd.print("Lembab: ");
+    lcd.print(ratalembab);
+    lcd.print("%");
+    delay(3000);
   }
-
-
 }
 
